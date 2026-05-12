7eb80c0922bdcdf311ca60b4ead3de85bf05e95d
Tom <tom.arruda009@gmail.com>
2026-05-12 13:55:07 -0300

    corrigindo script sql

--- BD02 5-5/BD2 14-4.sql
+++ BD02 5-5/BD2 14-4.sql
@@ -2,7 +2,7 @@ create database biblioteca;
 use biblioteca;
 
 create table Cliente(
-id_cliente int,
+id_cliente int AUTO_INCREMENT,
 nome varchar(40),
 email varchar(40),
 telefone varchar(20),
@@ -12,7 +12,7 @@ PRIMARY KEY (id_cliente)
 
 
 create table Funcionario(
-id_funcionario int,
+id_funcionario int AUTO_INCREMENT,
 nome varchar(40),
 cargo varchar(40),
 CPF char(11),
@@ -20,13 +20,13 @@ PRIMARY KEY (id_funcionario)
 );
 
 create table Autor(
-id_autor int,
+id_autor int AUTO_INCREMENT,
 nome_autor varchar(40),
 PRIMARY KEY (ID_Autor)
 );
 
 create table Livro(
-id_livro int,
+id_livro int AUTO_INCREMENT,
 ISBN int(13),
 titulo varchar(60),
 ano_publicacao int(4),
@@ -51,7 +51,7 @@ constraint fk_autor1 foreign key (id_autor) references Autor(id_autor)
 );
 
 create table emprestimo(
-id_emprestimo int,
+id_emprestimo int AUTO_INCREMENT,
 data_saida varchar(10),
 data_devolucao varchar(10),
 primary key (id_emprestimo),
